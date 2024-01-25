f896de17f1f22f9de2dd0e45e7a099b73044cd3f
Deepak-Maan <jattdevta1008@gmail.com>
2024-01-25 11:40:32 +0530

    GetInTouch section modified

--- src/components/GetInTouch.jsx
+++ src/components/GetInTouch.jsx
@@ -10,7 +10,7 @@ const GetInTouch = () => {
         <div className='absolute bottom-[10%] left-[10%] h-[40px] w-[40px] max-lg:hidden '><RightTriangle /></div>
         <div className='   px-5 sm:px-12 lg:pl-[118px] lg:pr-[90px]'>
           <h2 className=' text-2xl sm:text-3xl md:text-5xl lg:text-[64px] leading-6 md:leading-[44px] lg:leading-[88px] font-poppins pt-5 sm:pt-10 md:pt-14 lg:pt-20 font-semibold text-center mx-auto max-w-[1024px]'>We're here to make your website awesome.</h2>
-          <div className="flex justify-center pt-10 pb-5 md:pb-10 lg:pb-[88px] font-poppins leading-6 text-sm font-normal"><button className='py-3 px-4 text-white bg-[#B00000] flex rounded-lg'><Whatsapp /> Get  in touch with us</button></div>
+          <div className="flex justify-center pt-10 pb-5 md:pb-10 lg:pb-[88px] font-poppins leading-6 text-sm font-normal"><a href="whatsapp://send?phone=+919034394727" className='py-3 px-4 text-white bg-[#B00000] flex rounded-lg'><Whatsapp /> Get  in touch with us</a></div>
         </div>
       </div>
     </div>
