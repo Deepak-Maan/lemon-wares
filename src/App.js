1d76121711612f0853faf641bed2b44152f69684
Deepak-Maan <jattdevta1008@gmail.com>
2024-01-25 10:18:35 +0530

    added preloader

--- src/App.js
+++ src/App.js
@@ -2,13 +2,35 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Index from "./pages/Index";
 import Backtotop from "./components/BackToTop";
+import { useEffect, useState } from "react";
+import logo from "./assets/images/pageLogo.svg";
 function App() {
+  const [preload, setpreload] = useState(false);
+  useEffect(() => {
+    setpreload(true);
+    setTimeout(() => {
+      setpreload(false);
+    }, 4000);
+  }, []);
   return (
     <div>
-      <Backtotop />
-      <Routes>
-        <Route path="/" exact element={<Index />} />
-      </Routes>
+      {preload ? (
+        <div>
+          <div className="bg-white fixed top-0 bottom-0 overflow-hidden flex items-center flex-col justify-center w-full h-full">
+            <img className=" animate-bounce" src={logo} alt="gif" />
+            <div className=" pt-4 font-poppins text-[50px] text-[#B00000] font-semibold">
+              Loading ...
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <Backtotop />
+          <Routes>
+            <Route path="/" exact element={<Index />} />
+          </Routes>
+        </div>
+      )}
     </div>
   );
 }
